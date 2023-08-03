dc15ba9d461e08ab538d585fac15115b8108ef85
devikashendge <141109011+devikashendge@users.noreply.github.com>
2023-08-03 10:38:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
-var buttonColours = ["orange", "blue", "pink", "yellow"];
+var buttonColours = ["pink", "orange", "yellow", "blue"];
 
 //5. At the top of the game.js file, create a new empty array called gamePattern.
 var gamePattern = [];
